0528710341bca60086e790abc538bc649fb536dd
Clara Appiagyei <99737189+ClaraAppiagyei@users.noreply.github.com>
2022-05-12 15:55:35 +0200

    Add files via upload

---
+++ Javascript1/Week02/main.js
@@ -0,0 +1,82 @@
+//Assignment 1//
+
+console.log('Hello World'); //English//
+console.log('Oi, mundo'); //Portuguese//
+console.log('Hola, mundo'); //Spanish//
+console.log('Hej, verden'); //Danish//
+console.log('Hej, vårlden'); //Swedish//
+
+
+//Assignment 2//
+console.log(`I'm awesome`);
+
+//Assignment 3//
+let x;
+console.log(`the value of my variable x will be: undefined`);
+console.log(x);
+x=10;
+console.log(`the value of x will be: 10`);
+console.log(x);
+
+//Assignment 4//
+let y="My 1st string";
+console.log("the value of y is: 'My 1st string");
+y="My 2nd string";
+console.log("the value of y is: 'My 2nd string");
+console.log(y);
+
+//Assignment 5//
+let z=7.25;
+console.log(z);
+const a= Math.round(z);
+console.log(a);
+let highestNumber = a > z ? a : z;
+console.log(highestNumber);
+
+//Assignment 6//
+let things=[];
+console.log("the value of the array is gonna be empty");
+console.log(things);
+let animals=["Horse", "Cow", "Chicken"];
+console.log(animals);
+animals.push("baby pig");
+console.log(animals);
+
+//Assignment 7//
+let myString = "this is a test";
+console.log(myString); 
+console.log(myString.length);
+
+//Assignment 8//
+let myStr="Here are some Brazilian food classics";
+console.log("The value of my variable myStr is: " + myStr)
+let brazilianFoods=["Acarajé", "Feijoada", "Moqueca"];
+console.log("The value of my variable brazilianFoods is: " + brazilianFoods)
+let myInt=10;
+console.log("The value of my variable myInt is: " + myInt);
+let myBool=false;
+console.log("The value of my variable myBool is: " + myBool)
+console.log("I think typeof myStr is type: string");
+console.log(typeof myString);
+console.log("I think typeof brazilianFoods is type: array");
+console.log(typeof brazilianFoods);
+console.log("I think typeof myInt is type: integer");
+console.log(typeof myInt);
+console.log("I think typeof myBool is type: boolean");
+console.log(typeof myBool);
+
+if (typeof myInt === myBool) {
+    console.log("SAME TYPE");
+  } else {
+    console.log("NOT THE SAME TYPE");
+  }
+  
+  if (typeof myStr === brazilianFoods) {
+    console.log("SAME TYPE");
+  } else {
+    console.log("NOT THE SAME TYPE");
+  }
+
+  
+
+
